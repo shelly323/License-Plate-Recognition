861d675e897a94bcfac435d0c6123ca49f724abc
shelly323 <s411285021@gm.ntpu.edu.tw>
2026-06-10 21:07:19 +0800

    移除 PlateRecognizer 類別中的 normalize_char 和 template_matching 方法，簡化車牌辨識流程

--- utilis/recognizer.py
+++ utilis/recognizer.py
@@ -1,4 +1,3 @@
-# recognizer.py
 class PlateRecognizer:
     def __init__(self, template_dataset=None):
         a = 0
@@ -10,12 +9,6 @@ class PlateRecognizer:
 
     def segment_characters(self, cropped_plate):
         """依序切割出 6 個字元並處理『-』與『1』的特殊情況""" 
-
-    def normalize_char(self, char_img):
-        """將字元線性正規化成 15*45 大小""" 
-
-    def template_matching(self, norm_char):
-        """樣本比對法與二次比對機制""" 
 
     def recognize(self, plate_img):
         """一鍵辨識的 Pipeline"""
