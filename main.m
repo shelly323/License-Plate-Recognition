d0fa56c37ec004d9dd05434411cd222dd469711e
Sophie12321 <sophie200544@gmail.com>
2026-06-14 00:04:30 +0800

    新增主程式以初始化 PlateLocalizer，讀取影像並執行車牌偵測

---
+++ main.m
@@ -0,0 +1,19 @@
+plateDetector = PlateLocalizer();  %初始化
+% plateSegmenter = Segmenter();     % 還沒寫!!!!!
+
+file_path =  '.\image\sample 2026\';% 資料夾路徑
+img_path_list = dir(strcat(file_path,'*.jpg')); % jpg影像
+img_num = length(img_path_list); %照片數量
+
+if img_num > 0 
+        for j = 1:img_num 
+            image_name = img_path_list(j).name;
+            car_image =  imread(strcat(file_path,image_name)); %strcat:將多個字串合併
+            fprintf('正在處理 [%d/%d]: %s\n',j,strcat(file_path,image_name)); 
+            
+            %車牌偵測!!!!!!!!!!!!!!!
+            [img_clap, plate_location] = plateDetector.localizer(car_image);
+        end
+else
+    fprintf('在 %s 找不到任何 .jpg 檔案。\n', file_path);
+end
